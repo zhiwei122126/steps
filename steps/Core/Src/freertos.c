0cc0356fbd6ed63bb0e139cb29edc6313a28077f
zhiwei.zhang <zhiwei.zhang@hfxbo.cn>
2020-05-26 12:09:42 +0800

    cmsis rtos v2 api

--- steps/Core/Src/freertos.c
+++ steps/Core/Src/freertos.c
@@ -22,7 +22,7 @@
 #include "FreeRTOS.h"
 #include "task.h"
 #include "main.h"
-#include "cmsis_os.h"
+#include "cmsis_os2.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */     
