9efe68a70552b87e5b1e7d63f1f73d0f4bb9066e
zhiwei.zhang <zhiwei.zhang@hfxbo.cn>
2020-05-27 14:18:49 +0800

    100hz 系统时钟中断， malloc fail hook 和 栈溢出 hook。

--- steps/Core/Inc/FreeRTOSConfig.h
+++ steps/Core/Inc/FreeRTOSConfig.h
@@ -57,7 +57,7 @@
 #define configUSE_IDLE_HOOK                      0
 #define configUSE_TICK_HOOK                      0
 #define configCPU_CLOCK_HZ                       ( SystemCoreClock )
-#define configTICK_RATE_HZ                       ((TickType_t)50)
+#define configTICK_RATE_HZ                       ((TickType_t)100)
 #define configMAX_PRIORITIES                     ( 56 )
 #define configMINIMAL_STACK_SIZE                 ((uint16_t)512)
 #define configTOTAL_HEAP_SIZE                    ((size_t)61440)
@@ -67,7 +67,9 @@
 #define configUSE_16_BIT_TICKS                   0
 #define configUSE_MUTEXES                        1
 #define configQUEUE_REGISTRY_SIZE                8
+#define configCHECK_FOR_STACK_OVERFLOW           2
 #define configUSE_RECURSIVE_MUTEXES              1
+#define configUSE_MALLOC_FAILED_HOOK             1
 #define configUSE_COUNTING_SEMAPHORES            1
 #define configENABLE_BACKWARD_COMPATIBILITY      0
 #define configUSE_PORT_OPTIMISED_TASK_SELECTION  0
